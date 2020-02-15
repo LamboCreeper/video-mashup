5933e0fc024e3ff316407011544177013e5e205a
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-02-15 14:31:29 +0000

    Change it so that StorageService.upload() returns the path to the uploaded file.

--- services/StorageService.py
+++ services/StorageService.py
@@ -2,11 +2,14 @@ from google.cloud import storage
 
 
 class StorageService:
-    def __init__(self, bucket: str):
+    def __init__(self, bucket_name: str):
         self.client = storage.Client()
-        self.bucket = self.client.bucket(bucket)
+        self.bucket_name = bucket_name
+        self.bucket = self.client.bucket(bucket_name)
 
     def upload(self, source: str, destination: str):
         blob = self.bucket.blob(destination)
 
         blob.upload_from_filename(source)
+
+        return "gs://{}/{}".format(self.bucket_name, destination)
