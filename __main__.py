9f6a5d60431c26b07585deb5fb0f031432d0acdf
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-02-15 15:13:59 +0000

    Made it so it generates clips based on all the words in a video.

---
+++ __main__.py
@@ -0,0 +1,79 @@
+import json
+from sys import argv
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+from services.StorageService import StorageService
+from services.IntelligenceService import IntelligenceService
+from utils.create_clip import create_clip
+
+# Load config
+with open("config.json") as data:
+    json_data = json.load(data)
+    storage_bucket = json_data["storage_bucket"]
+
+# We don't care about the name of the file.
+args = argv
+args.remove(args[0])
+
+# Setup instances of the services.
+Storage = StorageService(storage_bucket)
+Intelligence = IntelligenceService()
+
+if len(args) == 3:
+    source = args[0]
+    destination = args[1]
+    sentence = args[2]
+
+    print("Attempting to create file at \"{}\" based on \"{}\" with sentence \"{}\"...".format(
+        destination,
+        source,
+        sentence
+    ))
+
+    try:
+        uploaded_file = Storage.upload(source, source)
+        transcriptions = Intelligence.transcript(uploaded_file)
+        original_video = VideoFileClip(source)
+
+        words_found = []
+        clips = []
+
+        for transcription in transcriptions:
+            for alternative in transcription.alternatives:
+                words = alternative.words
+                transcript = alternative.transcript
+                confidence = alternative.confidence
+
+                print("{}% confidence that transcript is \"{}\".".format(
+                    round(confidence * 100, 3),
+                    transcript
+                ))
+
+                for word in words:
+                    start = word.start_time
+                    end = word.end_time
+
+                    words_found.append({
+                        "word": word.word,
+                        "start": start.seconds + start.nanos * 1e-9,
+                        "end": end.seconds + end.nanos * 1e-9
+                    })
+
+        for word in words_found:
+            the_word = word["word"]
+            start = float(word["start"])
+            end = float(word["end"])
+
+            destination = "temp/{}".format(the_word)
+
+            clip = create_clip(destination, original_video, start, end)
+
+            clips.append({
+                "word": the_word,
+                "clio": clip
+            })
+    except IndexError:
+        print("Index Error occurred.")
+else:
+    print("You must supply a source, destination and sentence.")
+    print("Example: python3 speech-control my-video.mp4 final.mp4 \"Hello World\"")
