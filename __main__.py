63fb7494bb7657c73355ccbf296e5be68d8cfc22
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-02-20 15:15:32 +0000

    Added concatenation of clips.

--- __main__.py
+++ __main__.py
@@ -6,6 +6,7 @@ from moviepy.editor import concatenate_videoclips
 from services.StorageService import StorageService
 from services.IntelligenceService import IntelligenceService
 from utils.create_clip import create_clip
+from helpers.strip_punctuation import strip_punctuation
 
 # Load config
 with open("config.json") as data:
@@ -38,42 +39,69 @@ if len(args) == 3:
     clips = []
     words_found = []
 
-    for transcription in transcriptions:
-        for alternative in transcription.alternatives:
-            words = alternative.words
-            transcript = alternative.transcript
-            confidence = alternative.confidence
+    try:
+        for transcription in transcriptions:
+            for alternative in transcription.alternatives:
+                words = alternative.words
+                transcript = alternative.transcript
+                confidence = alternative.confidence
 
-            print("{}% confidence that transcript is \"{}\".".format(
-                round(confidence * 100, 3),
-                transcript
-            ))
+                print("{}% confidence that transcript is \"{}\".".format(
+                    round(confidence * 100, 3),
+                    transcript
+                ))
 
-            for word in words:
-                start = word.start_time
-                end = word.end_time
+                for word in words:
+                    start = word.start_time
+                    end = word.end_time
 
-                words_found.append({
-                    "word": word.word,
-                    "start": start.seconds + start.nanos * 1e-9,
-                    "end": end.seconds + end.nanos * 1e-9
-                })
+                    words_found.append({
+                        "word": word.word,
+                        "start": start.seconds + start.nanos * 1e-9,
+                        "end": end.seconds + end.nanos * 1e-9
+                    })
 
-    for word in words_found:
-        the_word = word["word"]
-        start = float(word["start"])
-        end = float(word["end"])
+        for word in words_found:
+            the_word = str(word["word"]).lower()
+            start = float(word["start"])
+            end = float(word["end"])
 
-        destination = "temp/{}".format(the_word)
+            the_word = strip_punctuation(the_word)
+            clip_destination = "temp/{}".format(the_word)
 
-        clip = create_clip(destination, original_video, start, end)
+            clip = create_clip(clip_destination, original_video, start, end)
 
-        clips.append({
-            "word": the_word,
-            "clip": clip
-        })
+            clips.append({
+                "word": the_word,
+                "clip": clip
+            })
+    except IndexError:
+        # This try/except block is only here so that all the videos are generated
+        # before we try to concatenate them. I can't think of a better way to deal
+        # with this right now.
 
-        print("Done!")
+        print("Index Error!")
+
+    required_videos = []
+
+    for given_word in sentence:
+        for clip in clips:
+            print("{} {}".format(given_word, clip["word"]))
+            if clip["word"] == given_word:
+                required_videos.append(clip["clip"])
+
+    final_video = concatenate_videoclips(required_videos)
+
+    final_video.write_videofile(
+        destination,
+        codec="libx264",
+        audio_codec="aac",
+        temp_audiofile="temp-audio.m4a",
+        remove_temp=True
+    )
+
+    # Clear out the temp directory
+    print("Done!")
 else:
     print("You must supply a source, destination and sentence.")
     print("Example: python3 speech-control my-video.mp4 final.mp4 \"Hello World\"")
