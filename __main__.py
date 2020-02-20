3e1838d8b380d6e5a524380a9743abe9846f51b0
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-02-20 11:19:13 +0000

    Moved try/except into create_clip until rather than __main__

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import json
 from sys import argv
 from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.editor import concatenate_videoclips
 
 from services.StorageService import StorageService
 from services.IntelligenceService import IntelligenceService
@@ -22,7 +23,7 @@ Intelligence = IntelligenceService()
 if len(args) == 3:
     source = args[0]
     destination = args[1]
-    sentence = args[2]
+    sentence = args[2].split(" ")
 
     print("Attempting to create file at \"{}\" based on \"{}\" with sentence \"{}\"...".format(
         destination,
@@ -30,50 +31,49 @@ if len(args) == 3:
         sentence
     ))
 
-    try:
-        uploaded_file = Storage.upload(source, source)
-        transcriptions = Intelligence.transcript(uploaded_file)
-        original_video = VideoFileClip(source)
+    uploaded_file = Storage.upload(source, source)
+    transcriptions = Intelligence.transcript(uploaded_file)
+    original_video = VideoFileClip(source)
 
-        words_found = []
-        clips = []
+    clips = []
+    words_found = []
 
-        for transcription in transcriptions:
-            for alternative in transcription.alternatives:
-                words = alternative.words
-                transcript = alternative.transcript
-                confidence = alternative.confidence
+    for transcription in transcriptions:
+        for alternative in transcription.alternatives:
+            words = alternative.words
+            transcript = alternative.transcript
+            confidence = alternative.confidence
 
-                print("{}% confidence that transcript is \"{}\".".format(
-                    round(confidence * 100, 3),
-                    transcript
-                ))
+            print("{}% confidence that transcript is \"{}\".".format(
+                round(confidence * 100, 3),
+                transcript
+            ))
 
-                for word in words:
-                    start = word.start_time
-                    end = word.end_time
+            for word in words:
+                start = word.start_time
+                end = word.end_time
 
-                    words_found.append({
-                        "word": word.word,
-                        "start": start.seconds + start.nanos * 1e-9,
-                        "end": end.seconds + end.nanos * 1e-9
-                    })
+                words_found.append({
+                    "word": word.word,
+                    "start": start.seconds + start.nanos * 1e-9,
+                    "end": end.seconds + end.nanos * 1e-9
+                })
 
-        for word in words_found:
-            the_word = word["word"]
-            start = float(word["start"])
-            end = float(word["end"])
+    for word in words_found:
+        the_word = word["word"]
+        start = float(word["start"])
+        end = float(word["end"])
 
-            destination = "temp/{}".format(the_word)
+        destination = "temp/{}".format(the_word)
 
-            clip = create_clip(destination, original_video, start, end)
+        clip = create_clip(destination, original_video, start, end)
 
-            clips.append({
-                "word": the_word,
-                "clio": clip
-            })
-    except IndexError:
-        print("Index Error occurred.")
+        clips.append({
+            "word": the_word,
+            "clip": clip
+        })
+
+        print("Done!")
 else:
     print("You must supply a source, destination and sentence.")
     print("Example: python3 speech-control my-video.mp4 final.mp4 \"Hello World\"")
--- utils/create_clip.py
+++ utils/create_clip.py
@@ -2,15 +2,18 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 
 
 def create_clip(destination: str, original_video: VideoFileClip, start: float, end: float, video_format="mp4"):
-    clip = original_video.subclip(start, end)
+    try:
+        clip = original_video.subclip(start, end)
 
-    clip.set_duration(end - start)
-    clip.write_videofile(
-        "{}.{}".format(destination, video_format),
-        codec="libx264",
-        audio_codec="aac",
-        temp_audiofile="temp-audio.m4a",
-        remove_temp=True
-    )
+        clip.set_duration(end - start)
+        clip.write_videofile(
+            "{}.{}".format(destination, video_format),
+            codec="libx264",
+            audio_codec="aac",
+            temp_audiofile="temp-audio.m4a",
+            remove_temp=True
+        )
 
-    return clip
+        return clip
+    except IndexError:
+        print("!! Error creating clip for {}".format(destination))
